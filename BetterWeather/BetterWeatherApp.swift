c2b59420c96a468e8d57f5ef40e0db3ce76d3f0b
Reid Smith <77130791+Retrix165@users.noreply.github.com>
2023-08-30 11:44:41 -0500

    Initial Commit

---
+++ BetterWeather/BetterWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterWeatherApp.swift
+//  BetterWeather
+//
+//  Created by Reid Smith on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
